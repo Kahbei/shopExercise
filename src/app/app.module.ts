742a3b1acaae8be55489290a948585d2f29ced15
Vincent Rabajoie <rabajoie.vincent@gmail.com>
2020-11-10 16:43:44 +0100

    eater_egg

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EasterEggComponent } from './components/easter-egg/easter-egg.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EasterEggComponent
   ],
   imports: [
     BrowserModule,
