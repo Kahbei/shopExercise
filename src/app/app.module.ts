ce9d9128a085ae5a460f06c9d4cfb3de527e5d73
Vincent Rabajoie <rabajoie.vincent@gmail.com>
2020-11-14 14:44:05 +0100

    ajout de panier et formulaire

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EasterEggComponent } from './components/easter-egg/easter-egg.component';
+import { PanierComponent } from './components/panier/panier.component';
+import { FormulaireComponent } from './components/formulaire/formulaire.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EasterEggComponent
+    EasterEggComponent,
+    PanierComponent,
+    FormulaireComponent
   ],
   imports: [
     BrowserModule,
