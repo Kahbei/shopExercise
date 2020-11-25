ccbcbf4260f826f65a022c942924867596f5b45c
Kahbei <mengus.gary@gmail.com>
2020-11-25 16:19:06 +0100

    une bouée pour vincent

--- src/app/components/panier/panier.component.ts
+++ src/app/components/panier/panier.component.ts
@@ -9,17 +9,18 @@ import { ShopService } from '../../shop.service';
   styleUrls: ['./panier.component.css'],
 })
 export class PanierComponent implements OnInit {
-  panierList: object = [
-    new Cheese('nom fromgae', 'test', 2, 'test'),
-    new Cheese('fromage2', '', 3, ''),
-  ];
   panier;
 
-  constructor(private http: ShopService) {
-    console.log(this.panier);
+  constructor(private http: ShopService) {}
+
+  getCartItems() {
+    this.http.getCart().subscribe((panier) => {
+      this.panier = panier;
+      console.log(this.panier);
+    });
   }
 
   ngOnInit(): void {
-    this.http.getCart().subscribe((panier) => (this.panier = panier));
+    this.getCartItems();
   }
 }
