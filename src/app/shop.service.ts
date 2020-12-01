ed31be61732c09b9e21cc8c17e146c6749747369
Vincent Rabajoie <rabajoie.vincent@gmail.com>
2020-12-01 18:02:03 +0100

    Ajout quantité fromages dans panier

--- src/app/shop.service.ts
+++ src/app/shop.service.ts
@@ -11,12 +11,17 @@ import { Cheese } from './model/cheese.model';
 export class ShopService {
   constructor(private http: HttpClient) {}
   panier = [];
+  dansPanier = false;
 
   getCheeses(): Observable<Cheese[]> {
     return of(CHEESE);
   }
 
   addToCart(cart) {
+    const Object = {
+      quantite:1,
+    }
+    cart.quantite = 1
     this.panier.push(cart)
     //console.log('hello ' + this.panier)
     return
@@ -26,7 +31,28 @@ export class ShopService {
     return this.panier
   }
 
-  getCart() {
-    // Va retourner la panier
-  }
+  // getCart() {
+    //   // Va retourner la panier
+    // }
+
+  /*
+  Avant d'ajouter dans le panier
+  Regarder s'il existe déjà
+  true = quantite++
+  false = on ajoute dans panier
+  */
+  verifDoubon(cart){
+    this.panier.forEach(function (value) {
+      if (value.name === cart.name) {
+        value.quantite++
+        this.dansPanier = true
+      }
+      
+    })
+    if (this.dansPanier === false) {
+      //mettre dans panier
+      this.addToCart(cart)
+    }
+
+    }
 }
